f67c5beafa9bf8f5f6f0b00e023201fc05b07e53
Eddy Nguyen <github@eddeee888.me>
2024-05-09 20:30:54 +1000

    Add graphql codegen + apollo setup

---
+++ codegen.ts
@@ -0,0 +1,40 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "https://beta.pokeapi.co/graphql/v1beta",
+  documents: "src/**/*.graphql",
+  hooks: {
+    afterAllFileWrite: ["prettier --write"],
+  },
+  generates: {
+    "src/graphql-types.generated.ts": {
+      documents: ["src/graphql/hirer/**/*.ts"],
+      plugins: ["typescript", "typescript-operations"],
+      config: {
+        namingConvention: {
+          enumValues: "change-case-all#upperCase",
+        },
+        scalars: {
+          ID: {
+            input: "string | number",
+            output: "string",
+          },
+        },
+      },
+    },
+    src: {
+      preset: "near-operation-file",
+      presetConfig: {
+        baseTypesPath: "./graphql-types.generated.ts",
+        extension: ".generated.ts",
+      },
+      plugins: ["typescript-operations", "typescript-react-apollo"],
+      config: {
+        enumsAsTypes: true,
+        documentMode: "documentNode",
+      },
+    },
+  },
+};
+
+export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const apolloClient = new ApolloClient({
+  uri: "https://beta.pokeapi.co/graphql/v1beta",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
